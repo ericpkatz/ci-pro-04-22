0ad25162c37fc978284a0de59ccbba343a528f13
ericpkatz <ericpkatz@gmail.com>
2020-04-23 06:44:45 -0400

    add sequelize

--- spec.js
+++ spec.js
@@ -1,21 +1,34 @@
+//app
 const express = require('express');
 const app = express();
-app.get('/', (req, res, next)=> res.send('foo'));
+
+//sequelize
+const Sequelize = require('sequelize');
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db');
+const User = conn.define('user', {
+  name: Sequelize.STRING
+});
+
+app.get('/', async(req, res, next)=> res.send(await User.findAll()));
 const _app = require('supertest')(app);
 
 
 const { expect } = require('chai');
-const foo = 'bar';
 
-describe('foo', ()=> {
-  it('equals bar', ()=> {
+describe('sanity check', ()=> {
+  const foo = 'bar';
+  it('foo equals bar', ()=> {
     expect(foo).to.equal('bar');
   });
 });
 
-describe('GET /foo', ()=> {
-  it('returns bar', ()=> {
+describe('GET /users', ()=> {
+  beforeEach(()=> conn.sync({ force: true}).then(()=> User.create({ name: 'Lucy'})));
+  it('returns a list of users', ()=> {
     return _app.get('/') 
-      .expect(200);
+      .expect(200)
+      .expect( response => {
+        expect(response.body.length).to.equal(1);
+      });
   });
 });
