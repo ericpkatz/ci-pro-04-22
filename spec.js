58cacb0d71fe91b784aa776b30675b0badbd7acb
ericpkatz <ericpkatz@gmail.com>
2020-04-22 21:50:46 -0400

    add route

--- spec.js
+++ spec.js
@@ -1,8 +1,21 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res, next)=> res.send('foo'));
+const _app = require('supertest')(app);
+
+
 const { expect } = require('chai');
 const foo = 'bar';
 
 describe('foo', ()=> {
   it('equals bar', ()=> {
     expect(foo).to.equal('bar');
+  });
+});
+
+describe('GET /foo', ()=> {
+  it('returns bar', ()=> {
+    return _app.get('/') 
+      .expect(200);
   });
 });
